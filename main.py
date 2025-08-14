86dcfaa9759638778455905b8d5d0cd1c0f0e85f
chanduluhar <116055746+chanduluhar@users.noreply.github.com>
2025-08-14 21:28:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,9 +26,5 @@ def achievements():
 def contact():
     return render_template('contact.html')
 
-# Vercel entry point
-def handler(event, context):
-    return app(event, context)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
