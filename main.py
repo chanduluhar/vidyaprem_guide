ba27880c6061cf3e7d88f954c631124d0148e25c
chanduluhar <116055746+chanduluhar@users.noreply.github.com>
2026-01-12 15:35:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# --- Page Routes ---
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -22,29 +23,9 @@ def team():
 def achievements():
     return render_template('achievements.html')
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-@app.route('/team')
-def team():
-    return render_template('team.html')
-
-@app.route('/achievements')
-def achievements():
-    return render_template('achievements.html')
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
 
 @app.route("/scholarship")
 def scholarship():
@@ -53,5 +34,15 @@ def scholarship():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+
+
+
+
+
+
+
+
 
 
